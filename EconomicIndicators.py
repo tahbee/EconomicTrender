291817aa8963bc45866039aa06494f118d194804
Tahbee Hassan <56143567+tahbee@users.noreply.github.com>
2022-01-26 13:12:01 -0500

    Showing line graphs, box plots of economic indicators and writing csv file for these categories separately

--- EconomicIndicators.py
+++ EconomicIndicators.py
@@ -6,43 +6,120 @@ import csv
 import datetime
 from pylab import *
 
-def gdpData():
-    gdpTimeStampValues = []
-    gdpDataValues = []
+def economicIndicatorsData(dataOption):
+    economicIndicatorsTimeStampValues = []
+    economicIndicatorsDataValues = []
 
-    gdpAPIUrl = 'https://www.alphavantage.co/query?function=REAL_GDP&interval=annual&apikey=J4RQ2HAVH7OZNSRM'
-    gdpDataFeedResponse = requests.get(gdpAPIUrl)
-    gdpData = gdpDataFeedResponse.json()
-
-    gdpJSONArray = gdpData["data"]
-
-    for gdpJSONObject in gdpJSONArray:
-        gdpTimeStampValues.append(gdpJSONObject["date"])
-
-        gdpDataValues.append(float(gdpJSONObject["value"]))
-
-
-
-    print(gdpTimeStampValues)
-    print(gdpDataValues)
+    economicIndicatorsAPIUrl = ''
     
-    xAxisCoordinates = numpy.array([ x for x in range(5) ])
-    yAxisCoordinates = numpy.array(gdpDataValues[:5])
+    if (dataOption == 1):
+        economicIndicatorsAPIUrl = 'https://www.alphavantage.co/query?function=REAL_GDP&interval=annual&apikey=J4RQ2HAVH7OZNSRM'
+        
+    elif (dataOption == 4):
+        economicIndicatorsAPIUrl = 'https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey=J4RQ2HAVH7OZNSRM'
+        
+    elif (dataOption == 2):
+        economicIndicatorsAPIUrl = 'https://www.alphavantage.co/query?function=FEDERAL_FUNDS_RATE&interval=monthly&apikey=J4RQ2HAVH7OZNSRM'
+        
+    elif (dataOption == 3):
+        economicIndicatorsAPIUrl = 'https://www.alphavantage.co/query?function=INFLATION&apikey=demo'
 
-    bestFitLine1, bestFitLine2 = np.polyfit(xAxisCoordinates, yAxisCoordinates, 1)
+    economicIndicatorsDataFeedResponse = requests.get(economicIndicatorsAPIUrl)
+    economicIndicatorsData = economicIndicatorsDataFeedResponse.json()
 
-    pythonPlot.scatter(xAxisCoordinates, yAxisCoordinates, color='red')
+    economicIndicatorsJSONArray = economicIndicatorsData["data"]
 
-    pythonPlot.plot(xAxisCoordinates, bestFitLine1*xAxisCoordinates+bestFitLine2, color='green', linestyle ='--', linewidth = 3)
+    for economicIndicatorsJSONObject in economicIndicatorsJSONArray:
+        economicIndicatorsTimeStampValues.append(economicIndicatorsJSONObject["date"])
 
-    pythonPlot.text(1, 17, 'y = ' + '{:.2f}'.format(bestFitLine2) + ' + {:.2f}'.format(bestFitLine1) + 'x', size=14)
+        economicIndicatorsDataValues.append(float(economicIndicatorsJSONObject["value"]))
 
+    economicIndicatorsTimeStampValues.reverse()
+    economicIndicatorsDataValues.reverse()
+    pythonPlot.plot(economicIndicatorsTimeStampValues[-10:], economicIndicatorsDataValues[-10:], color='green', marker='o') #retrieve last 10 elements of list
 
+    if (dataOption == 1):
+        pythonPlot.title('Line Graph of Annual Real GDP of US per year for last 10 years (Download the CSV file at the end to see complete data of all the years in the past)', fontsize=10)
+        
+    elif (dataOption == 4):
+        pythonPlot.title('Line Graph of Monthly Unemployment Rate Of US for last 10 months (Download the CSV file at the end to see complete data of all the years in the past)', fontsize=10)
+        
+    elif (dataOption == 2):
+        pythonPlot.title('Line Graph of Monthly Interest Rate Of US for last 10 months (Download the CSV file at the end to see complete data of all the years in the past)', fontsize=10)
 
+    elif (dataOption == 3):
+        pythonPlot.title('Line Graph of Annual Inflation Rate Of US per year for last 10 years (Download the CSV file at the end to see complete data of all the years in the past)', fontsize=10)
 
-dataFeedOptionInput = input ("This program shows the data containing the annual and quarterly Real GDP of US, Monthly interest rates of US, Annual Inflation rates of US and Monthly Unemployment data of US. Do you want to see it? 1 or 0: ")
+        
+    pythonPlot.xlabel('Date', fontsize=10)
+    pythonPlot.ylabel('Value', fontsize=10)
+    pythonPlot.grid(True)
+    pythonPlot.show()
+
+    boxPlotFigure, axisCoordinates = pythonPlot.subplots()
+    
+    pandasDataFrame = pandas.DataFrame({'Value': economicIndicatorsDataValues})
+
+    axisCoordinates = pandasDataFrame.boxplot(column=['Value'], figsize=(15,5), grid=True)
+
+    if (dataOption == 1):
+        axisCoordinates.set_title('Box Plot For Annual Real GDP of US per year')
+        
+    elif (dataOption == 4):
+        axisCoordinates.set_title('Box Plot For Monthly Unemployment rates of US')
+        
+    elif (dataOption == 2):
+        axisCoordinates.set_title('Box Plot For Monthly Interest rates of US')
+        
+    elif (dataOption == 3):
+        axisCoordinates.set_title('Box Plot For Annual Inflation rates of US')
+
+    pythonPlot.show()
+
+    csvDownloadPrompt = input("Do you want to download the entire dataset? (y/n): ")
+
+    if (csvDownloadPrompt == 'y'):
+        try:
+            economicIndicatorsCSVHeader = ['Date','Value']
+
+            currentTime = datetime.datetime.now()
+
+            fileNameToSave = ''
+
+            if (dataOption == 1):
+                fileNameToSave = 'Annual_US_GDP_'+str(currentTime)+'.csv'
+
+            elif (dataOption == 4):
+                fileNameToSave = 'Monthly_Unemployment_Data_'+str(currentTime)+'.csv'
+
+            elif (dataOption == 2):
+                fileNameToSave = 'Monthly_Interest_Rate_'+str(currentTime)+'.csv'
+
+            elif (dataOption == 3):
+                fileNameToSave = 'Annual_Inflation_Rate_'+str(currentTime)+'.csv'
+    
+            with open(fileNameToSave, 'w', encoding="utf-8") as economicIndicatorsCSVFile:
+                economicIndicatorsCSVWriter = csv.writer(economicIndicatorsCSVFile, delimiter='\t')
+
+                economicIndicatorsCSVWriter.writerow(economicIndicatorsCSVHeader)
+
+                for economicIndicatorItem in zip(economicIndicatorsTimeStampValues,economicIndicatorsDataValues):
+                    economicIndicatorsCSVWriter.writerow(economicIndicatorItem)
+
+            print("File successfully saved. Please check your Documents, Downloads or any other folders in C drive or Finder where you set the default download path.")        
+        except:
+            print("An exception occurred")
+            
+
+dataFeedOptionInput = input ("This program shows the data containing the following options: 1) Annual Real GDP of US, 2) Monthly interest rates of US, 3) Annual Inflation rates of US and 4) Monthly Unemployment data of US. Enter an option number to continue: ")
     
 if (int(dataFeedOptionInput) == 1):
-    gdpData()
+    economicIndicatorsData(1)
+elif (int(dataFeedOptionInput) == 2):
+    economicIndicatorsData(2)
+elif (int(dataFeedOptionInput) == 3):
+    economicIndicatorsData(3)
+elif (int(dataFeedOptionInput) == 4):
+    economicIndicatorsData(4)    
 else:
   print("End Of Program")    
